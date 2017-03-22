af914ec31a3b5664055ea32d7763cb50b98a966a
kephax0 <kephax@live.com>
2017-03-22 20:59:38 +0100

    Project to repository

---
+++ src/Apl.java
@@ -0,0 +1,22 @@
+
+public class Apl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SearchTree<Integer, String> st = new SearchTree<Integer,String>();
+		st.put(6, "F");
+		st.put(2, "B");
+		st.put(7, "G");
+		st.put(1, "A");
+		st.put(4, "D");
+		st.put(3, "C");
+		st.put(5, "E");
+		
+		System.out.println(st.)
+		
+	}
+
+}
